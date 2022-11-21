02dad507e64294beb5dd18820b26d24d08365c4d
olejorga <olejorga@icloud.com>
2022-11-21 18:23:51 +0100

    FIX: Cleanup!

--- oppgave_3/types/result.ts
+++ oppgave_3/types/result.ts
@@ -1,5 +1,12 @@
-export type Data<T> = { status: number; data: T }
-export type Error = { status: number; message: string }
+export type Data<T> = {
+  status: number
+  data: T
+}
+
+export type Error = {
+  status: number
+  message: string
+}
 
 export type Result<T> = Data<T> | Error
 export type ResultAsync<T> = Promise<Result<T>>
