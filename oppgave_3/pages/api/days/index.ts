2972a40a3d1021afa72d8dc330855bc07a94bcdc
Erik Jarem <erik.jarem@gmail.com>
2022-11-21 16:24:40 +0100

    Fix: Typo

--- oppgave_3/pages/api/days/index.ts
+++ oppgave_3/pages/api/days/index.ts
@@ -4,13 +4,13 @@ import { Result } from '../../../types/result'
 
 export default async function daysHandler(
   req: NextApiRequest,
-  res: NextApiResponse<Result<Day[]>
+  res: NextApiResponse<Result<Day[]>>
 ) {
   const { method } = req
 
   switch (method?.toUpperCase()) {
     case 'GET':
-        return res.status(200)
+      return res.status(200)
     default:
       return res
         .status(405)
