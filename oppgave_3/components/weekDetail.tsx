62eade4ef5c62f0c5d49daf57c7f9ee4d78cdba7
Erik Jarem <erik.jarem@gmail.com>
2022-11-22 11:33:37 +0100

    FEAT: WeekDetail page.

---
+++ oppgave_3/components/weekDetail.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { Day, Week } from '../types/model'
+
+type WeekProps = {
+  week: Week
+}
+
+export default function WeekDetail({ week }: WeekProps) {
+  const [buttonText, setButtonText] = useState('Se dager')
+  const [hidden, setHidden] = useState(true)
+
+  useEffect(() => {
+    console.log('hei')
+    if (hidden) setButtonText('Se dager')
+    else setButtonText('Lukk dager')
+  }, [hidden])
+
+  const handleClick = () => {
+    setHidden(!hidden)
+  }
+
+  return (
+    <div className="weekDetailBox">
+      <h2 className="weekDetailTitle">Uke {week.number.toString()}</h2>
+      <ul hidden={hidden}>
+        {week.days?.map((d) => (
+          <li key={d.id} className="daysList">
+            <span>{d.name}</span>
+            <span>{d.employeeId}</span>
+          </li>
+        ))}
+      </ul>
+      <p className="weekDetailToggle" onClick={handleClick}>
+        {buttonText}
+      </p>
+    </div>
+  )
+}
--- oppgave_3/pages/index.tsx
+++ oppgave_3/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import WeekList from '../components/weekList'
 
 const Home: NextPage = () => {
   return (
     <main>
       <h1>Lunsjkalender</h1>
+      <WeekList></WeekList>
     </main>
   )
 }
