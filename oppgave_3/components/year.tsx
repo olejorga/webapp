7bda98d07c311689dcf4f77cf243dcf022348e7e
Erik Jarem <erik.jarem@gmail.com>
2022-11-22 12:58:30 +0100

    FIX: Added a header for year.ts and margin for titles

--- oppgave_3/components/year.tsx
+++ oppgave_3/components/year.tsx
@@ -3,6 +3,7 @@ const weekTest = Array.from({ length: 52 }, (_, i) => i + 1)
 export default function Year() {
   return (
     <>
+      <h2 className="yearTitle">Uker</h2>
       <ul className="year">
         {weekTest.map((oneweek) => (
           <Week key={oneweek} week={oneweek} />
