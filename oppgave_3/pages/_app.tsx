e52c9b7637327fc2677c359c58a950fc1e5d709c
olejorga <olejorga@icloud.com>
2022-11-24 15:36:17 +0100

    FEAT: Updated contexts.

--- oppgave_3/pages/_app.tsx
+++ oppgave_3/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/main.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function LunchApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
@@ -10,4 +10,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-export default MyApp
+export default LunchApp
