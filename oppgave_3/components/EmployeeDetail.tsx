e0e488b30c535fb6b3884a403512bf42ef366913
olejorga <olejorga@icloud.com>
2022-11-26 22:00:51 +0100

    FEAT: Added overview over overrides!

--- oppgave_3/components/EmployeeDetail.tsx
+++ oppgave_3/components/EmployeeDetail.tsx
@@ -21,18 +21,48 @@ export default function EmployeeDetail({ employee, expanded }: EmployeeProps) {
         </Link>
       </nav>
       {open && (
-        <Table>
-          {employee.days?.map((day) => (
-            <Row key={day.id}>
-              <Column>{day.name}</Column>
-              <Column>
-                <Link href={'/weeks/' + day.week?.number}>
-                  <a className="underline">{`Uke ${day.week?.number}`}</a>
-                </Link>
-              </Column>
-            </Row>
-          ))}
-        </Table>
+        <>
+          {employee.overrides && employee.overrides.length > 0 && (
+            <>
+              <h3 className="mb-2 text-lg font-bold">Ekstra</h3>
+              <Card>
+                <Table>
+                  {employee.overrides?.map((day) => (
+                    <Row key={day.id}>
+                      <Column>{day.name}</Column>
+                      <Column>
+                        <Link href={'/weeks/' + day.week?.number}>
+                          <a className="underline">{`Uke ${day.week?.number}`}</a>
+                        </Link>
+                      </Column>
+                    </Row>
+                  ))}
+                </Table>
+              </Card>
+              <hr className="mt-8 mb-6" />
+            </>
+          )}
+
+          <h3 className="text-lg font-bold">Planlagt</h3>
+          <Table>
+            {employee.days?.map((day) => (
+              <Row key={day.id}>
+                <Column>
+                  <span className={day.overrideId ? 'line-through' : ''}>
+                    {day.name}
+                  </span>
+                </Column>
+                <Column>
+                  <Link href={'/weeks/' + day.week?.number}>
+                    <a
+                      className={day.overrideId ? 'line-through' : 'underline'}
+                    >{`Uke ${day.week?.number}`}</a>
+                  </Link>
+                </Column>
+              </Row>
+            ))}
+          </Table>
+        </>
       )}
       {!expanded && (
         <button className="mt-4 underline" onClick={() => setOpen(!open)}>
